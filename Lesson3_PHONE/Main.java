8150ee8139b471dd04a72d86273b3a62116c8444
d.lukin <d.lukin@x5.ru>
2020-09-09 19:20:02 +0300

    Задание Phone

--- Lesson3_PHONE/Main.java
+++ Lesson3_PHONE/Main.java
@@ -3,7 +3,7 @@ package Lesson3_PHONE;
 import java.util.Date;
 
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Phone phone1 = new Phone("N234","Nokia A310",200);
         Phone phone2 = new Phone("N345","Ericson XXL",550);
         Phone phone3 = new Phone("N678","Gnusmas 10A",150);
@@ -11,6 +11,6 @@ public class Main {
         System.out.println(phone2.model + " " +phone2.number + " " + phone2.weght);
         System.out.println(phone3.model + " " +phone3.number + " " + phone3.weght);
 
-       // phone1.getNumber();
+        phone1.getNumber();
     }
 }
