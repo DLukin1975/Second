efd6bdda5090804fe39004902ecbdc319f0fef8a
d.lukin <d.lukin@x5.ru>
2020-09-09 18:35:30 +0300

    Задание Phone

--- Lesson3_PHONE/Phone.java
+++ Lesson3_PHONE/Phone.java
@@ -1,16 +1,19 @@
 package Lesson3_PHONE;
 
 public class Phone {
-    Phone(String number, String model, int weght) {
-        this.number = number;
-        this.model = model;
-        this.weght = 0;
-    }
 
     Phone(String number, String model) {
         this.number = number;
         this.model = model;
     }
+
+    Phone(String number, String model, int weght) {
+        this.number = number;
+        this.model = model;
+        this.weght = weght;
+    }
+
+
 
     Phone() {
     }
