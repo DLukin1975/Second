e0b81ac41ace8dea5d2893aff2c48f3ab8d91480
d.lukin <d.lukin@x5.ru>
2020-09-09 19:24:26 +0300

    Задание Phone

--- Lesson3_PHONE/Main.java
+++ Lesson3_PHONE/Main.java
@@ -7,10 +7,15 @@ public class Main {
         Phone phone1 = new Phone("N234","Nokia A310",200);
         Phone phone2 = new Phone("N345","Ericson XXL",550);
         Phone phone3 = new Phone("N678","Gnusmas 10A",150);
+        System.out.println("Созданные экземпляры");
         System.out.println(phone1.model + " " +phone1.number + " " + phone1.weght);
         System.out.println(phone2.model + " " +phone2.number + " " + phone2.weght);
         System.out.println(phone3.model + " " +phone3.number + " " + phone3.weght);
 
+        System.out.println();
+        System.out.println("Вызов метода getNumber");
         phone1.getNumber();
+        phone2.getNumber();
+        phone3.getNumber();
     }
 }
