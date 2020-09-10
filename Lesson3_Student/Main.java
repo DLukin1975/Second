c909a66775335c7ad7248d5a4e231af99bff71b7
d.lukin <d.lukin@x5.ru>
2020-09-10 18:54:49 +0300

    Обновление Student

--- Lesson3_Student/Main.java
+++ Lesson3_Student/Main.java
@@ -4,12 +4,12 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Dmitriy", "Student", "604", 5d);
-        System.out.println(student.getScholarship());
-        Student aspirant = new Aspirant("Dmitriy", "Aspirant", "604", 5d);
-        System.out.println(aspirant.getScholarship());
-        Student[] student1 = {new Student("Dmitriy", "Student", "604", 5d),
+        Student[] student = {new Student("Dmitriy", "Student", "604", 5d),
                               new Aspirant("Dmitriy", "Aspirant", "604", 5d)};
+        int i;
+        for (i=0;i< student.length;i++) {
+            System.out.println(student[i].getScholarship());
+        }
 
     }
 }
