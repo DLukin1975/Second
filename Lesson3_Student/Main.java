37abe19749b756a4bb46b8aaa884f32e7809762d
d.lukin <d.lukin@x5.ru>
2020-09-10 18:59:19 +0300

    Обновление Student

--- Lesson3_Student/Main.java
+++ Lesson3_Student/Main.java
@@ -8,7 +8,8 @@ public class Main {
                               new Aspirant("Dmitriy", "Aspirant", "604", 5d)};
         int i;
         for (i=0;i< student.length;i++) {
-            System.out.println(student[i].getScholarship());
+            System.out.println("У " + student[i].firstName +"." + student[i].lastName +
+                    " стипендия " + student[i].getScholarship());
         }
 
     }
