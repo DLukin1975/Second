3ef3d46637034c7a2dc499182c2ab9a8200102ac
d.lukin <d.lukin@x5.ru>
2020-09-10 17:35:50 +0300

    Обновление Animal

--- Lesson3_Animal/Main.java
+++ Lesson3_Animal/Main.java
@@ -2,21 +2,15 @@ package Lesson3_Animal;
 
 public class Main {
     public void main(String[] args) {
-        Animal animal = new Animal("Bear","Малина","Сибирь");
-        Animal animal2 = new Animal("Egle","кролик","степь");
-        System.out.println(animal.name);
-        System.out.println(animal2.name);
-        animal.sleep();
-        animal.eat();
-        animal.makeNoise();
+        int i;
+        DrDullitle drDullitle = new DrDullitle();
         Animal[] animalsQueue = {new Animal("Bear","Малина","Сибирь")
                 ,new Dog("Шарик", "мясо", "конура")
                 ,new Cat("Барсик", "рыбка", "диван")
                 ,new Horse("Холстомер","сено", "конюшня")
         };
-     //   animalsQueue[0] = new Animal("Bear","Малина","Сибирь");
-     //   animalsQueue[1] = new Dog("Шарик", "мясо", "конура");
-      //  animalsQueue[2] = new Cat("Барсик", "рыбка", "диван");
-     //   animalsQueue[3] = new Horse("Холстомер","сено", "конюшня");
+        for (i=0;i<animalsQueue.length;i++) {
+            drDullitle.treatAnimal(animalsQueue[i]);
+        }
     }
 }
