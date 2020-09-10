82eebd3fd75d8965e8646bd79672c49bf2c8dc19
d.lukin <d.lukin@x5.ru>
2020-09-10 15:02:03 +0300

    Задание Animal

--- Lesson3_Animal/Main.java
+++ Lesson3_Animal/Main.java
@@ -2,9 +2,10 @@ package Lesson3_Animal;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal("Bear");
-        Animal animal2 = new Animal("Egle");
+        Animal animal = new Animal("Bear","Малина","Сибирь");
+        Animal animal2 = new Animal("Egle","кролик","степь");
         System.out.println(animal.name);
         System.out.println(animal2.name);
+        animal.sleep();
     }
 }
