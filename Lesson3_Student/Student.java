71f0eaaf1af7d6aa7f34de95c1541d83172a3cc1
d.lukin <d.lukin@x5.ru>
2020-09-10 18:23:54 +0300

    Обновление Student

--- Lesson3_Student/Student.java
+++ Lesson3_Student/Student.java
@@ -1,7 +1,25 @@
 package Lesson3_Student;
 
 public class Student {
-    public Student (String firstName, String lastName, String group, double averageMark) {
+    double averageMark;
+    String firstName;
+    String lastName;
+    String group;
 
+    public Student (String firstName, String lastName, String group, double averageMark) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+    }
+    public double getScholarship() {
+       double shcolarship;
+       if (averageMark == 5d) {
+           shcolarship = 100d;
+        }
+       else {
+           shcolarship = 80d;
+        }
+       return shcolarship;
     }
 }
