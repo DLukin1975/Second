47a6c827c576afdc7e211d3c5ddadd43debb119d
d.lukin <d.lukin@x5.ru>
2020-09-13 21:46:11 +0300

    Урок 4 задание 1

--- Lesson4Task1/Main.java
+++ Lesson4Task1/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Shape[] shape = new Shape[3];
         shape[0] = new Triangle();
         shape[1] = new Circle();
-        shape[2] = new Rectangle();
+        shape[2] = new Rectangle(10);
         for (Shape sh : shape) {
             System.out.println("Площадь фигуры: " + sh.square());
         }
--- Lesson4Task1/Rectangle.java
+++ Lesson4Task1/Rectangle.java
@@ -1,8 +1,16 @@
 package Lesson4Task1;
 
 public class Rectangle implements  Shape {
+    private int x;
+
+//    private int x;
+
+    public Rectangle (int x) {
+  //     super(x);
+    }
     @Override
     public double square() {
-        return 0;
+        //int x = this.x;
+        return x;
     }
 }
