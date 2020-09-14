316647b3f35cef8410c953c3fb4afb52c0bfb8e1
d.lukin <d.lukin@x5.ru>
2020-09-14 21:30:40 +0300

    Урок 3

--- Lesson3_Student/Student.java
+++ Lesson3_Student/Student.java
@@ -6,20 +6,20 @@ public class Student {
     String lastName;
     String group;
 
-    public Student (String firstName, String lastName, String group, double averageMark) {
+    public Student(String firstName, String lastName, String group, double averageMark) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.group = group;
         this.averageMark = averageMark;
     }
+
     public double getScholarship() {
-       double shcolarship;
-       if (averageMark == 5d) {
-           shcolarship = 100d;
+        double shcolarship;
+        if (averageMark == 5d) {
+            shcolarship = 100d;
+        } else {
+            shcolarship = 80d;
         }
-       else {
-           shcolarship = 80d;
-        }
-       return shcolarship;
+        return shcolarship;
     }
 }
