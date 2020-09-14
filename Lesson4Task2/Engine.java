8c5b0035c7ad2ff7bc9e820aff8d45542491ad58
d.lukin <d.lukin@x5.ru>
2020-09-14 21:20:22 +0300

    Урок 4

--- Lesson4Task2/Engine.java
+++ Lesson4Task2/Engine.java
@@ -3,7 +3,8 @@ package Lesson4Task2;
 public class Engine {
     public String produser;
     public Integer power;
-    public Engine (String produser, Integer power) {
+
+    public Engine(String produser, Integer power) {
         this.produser = produser;
         this.power = power;
     }
