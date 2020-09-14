011d687044791de9e9da2d80852b18db73a028c0
d.lukin <d.lukin@x5.ru>
2020-09-14 21:21:43 +0300

    Урок 4

--- Lesson4Task1/Circle.java
+++ Lesson4Task1/Circle.java
@@ -1,12 +1,14 @@
 package Lesson4Task1;
 
 public class Circle implements Shape {
-    private  double r;
-    public  Circle (double r) {
+    private double r;
+
+    public Circle(double r) {
         this.r = r;
     }
+
     @Override
     public double square() {
-        return this.r*PI/2;
+        return this.r * PI / 2;
     }
 }
