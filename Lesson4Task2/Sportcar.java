9610e50a240aac2eae216617f195aa5f7390db37
d.lukin <d.lukin@x5.ru>
2020-09-14 21:19:37 +0300

    Урок 4

--- Lesson4Task2/Sportcar.java
+++ Lesson4Task2/Sportcar.java
@@ -1,26 +1,28 @@
 package Lesson4Task2;
 
-public class Sportcar extends  Car  {
-    public Sportcar (String markAuto, String classAuto, Engine engine) {
+public class Sportcar extends Car {
+    public Sportcar(String markAuto, String classAuto, Engine engine) {
         super(markAuto, classAuto, engine);
-        }
-@Override
-public void start () {
-        System.out.println("Спорткар поехал");
-        }
+    }
 
-@Override
-public void stop () {
+    @Override
+    public void start() {
+        System.out.println("Спорткар поехал");
+    }
+
+    @Override
+    public void stop() {
         System.out.println("Спорткар остановился");
-        }
-@Override
-public void printinfo () {
+    }
+
+    @Override
+    public void printinfo() {
         String markAuto = super.markAuto;
         String classAuto = super.classAuto;
         Engine engine = super.engine;
         System.out.println("Марка " + markAuto);
         System.out.println("Класс " + classAuto);
-        System.out.println ("Производитель двигателя" + engine.produser);
-        System.out.println ("Мощность двигателя" + engine.power);
-        }
-        }
+        System.out.println("Производитель двигателя" + engine.produser);
+        System.out.println("Мощность двигателя" + engine.power);
+    }
+}
