003e4414681b66ae6d366ab0f00117fadf6c2089
d.lukin <d.lukin@x5.ru>
2020-09-14 21:31:16 +0300

    Урок 3

--- Lesson3_Student/Aspirant.java
+++ Lesson3_Student/Aspirant.java
@@ -1,20 +1,22 @@
 package Lesson3_Student;
 
-public class Aspirant extends Student{
+public class Aspirant extends Student {
     String aspirantWork;
+
     public Aspirant(String firstName, String lastName, String group, double averageMark) {
         super(firstName, lastName, group, averageMark);
     }
+
     public Aspirant(String firstName, String lastName, String group, double averageMark, String aspirantWork) {
         super(firstName, lastName, group, averageMark);
         this.aspirantWork = aspirantWork;
     }
+
     public double getScholarship() {
         double shcolarship;
         if (averageMark == 5d) {
             shcolarship = 200d;
-        }
-        else {
+        } else {
             shcolarship = 180d;
         }
         return shcolarship;
