e8b0f1876e8eacc6e6b403b8c52520bcf7eba73e
d.lukin <d.lukin@x5.ru>
2020-09-14 21:13:54 +0300

    Урок 2 задание 4

--- Lesson2task4.java
+++ Lesson2task4.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Lesson2task4 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println ("Введите целое число");
+        System.out.println("Введите целое число");
         int number = in.nextInt();
         if (number > 0) {
-            number=++number;
-        }
-        else if (number<0) {
-            number=number-2;
-        }
-        else number=10;
+            number = ++number;
+        } else if (number < 0) {
+            number = number - 2;
+        } else number = 10;
         System.out.print(number);
     }
 }
