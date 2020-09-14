89c6be4bf6500a0b66a104ec2f40922e83c2d614
d.lukin <d.lukin@x5.ru>
2020-09-14 18:01:42 +0300

    Урок 4 задание 2

---
+++ Lesson4Task2/Car.java
@@ -0,0 +1,12 @@
+package Lesson4Task2;
+
+public class Car {
+    private String markAuto;
+    private String classAuto;
+    private Engine engine;
+    public Car (String markAuto, String classAuto, Engine engine) {
+        this.markAuto = markAuto;
+        this.classAuto = classAuto;
+        this.engine = engine;
+    }
+}
