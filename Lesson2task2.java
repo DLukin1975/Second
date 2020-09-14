489431ba9188ccf1f9fc8931d2fd21f52075418c
d.lukin <d.lukin@x5.ru>
2020-09-14 21:11:40 +0300

    Урок 2 задание 2

--- Lesson2task2.java
+++ Lesson2task2.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
-//import java.util.Math;
+
 public class Lesson2task2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int digit  = 4;
-        System.out.println ("Введите " + digit + " значное число");
+        int digit = 4;
+        System.out.println("Введите " + digit + " значное число");
         int number = in.nextInt();
-        if ((number < Math.pow(10,digit-1))||(number > (Math.pow(10,digit)-1))) {
-            System.out.println ("Число не является " + digit +" значным");
-        }
-        else {
+        if ((number < Math.pow(10, digit - 1)) || (number > (Math.pow(10, digit) - 1))) {
+            System.out.println("Число не является " + digit + " значным");
+        } else {
             int i;
             int result = 0;
             int pocket = number;
-            for (i=0;digit>i;i++) {
-                result = result + pocket%10;
-                pocket = (pocket - pocket%10)/10;
+            for (i = 0; digit > i; i++) {
+                result = result + pocket % 10;
+                pocket = (pocket - pocket % 10) / 10;
             }
             System.out.print("Сумма цифр - ");
             System.out.println(result);
-  //          System.out.println(Math.pow(2,3));
         }
     }
 }
