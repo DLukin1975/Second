6b195482a9be97f08c4768742725d4412b6f6641
d.lukin <d.lukin@x5.ru>
2020-09-14 21:20:03 +0300

    Урок 4

--- Lesson4Task2/Lorry.java
+++ Lesson4Task2/Lorry.java
@@ -1,26 +1,28 @@
 package Lesson4Task2;
 
-public class Lorry  extends Car {
-    public Lorry (String markAuto, String classAuto, Engine engine) {
+public class Lorry extends Car {
+    public Lorry(String markAuto, String classAuto, Engine engine) {
         super(markAuto, classAuto, engine);
-        }
-        @Override
-        public void start () {
-        System.out.println("Грузовик поехал");
-        }
+    }
 
-        @Override
-             public void stop () {
-            System.out.println("Грузовик остановился");
-        }
-        @Override
-            public void printinfo () {
-            String markAuto = super.markAuto;
-            String classAuto = super.classAuto;
-            Engine engine = super.engine;
-            System.out.println("Марка " + markAuto);
-            System.out.println("Класс " + classAuto);
-            System.out.println ("Производитель двигателя" + engine.produser);
-            System.out.println ("Мощность двигателя" + engine.power);
+    @Override
+    public void start() {
+        System.out.println("Грузовик поехал");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Грузовик остановился");
+    }
+
+    @Override
+    public void printinfo() {
+        String markAuto = super.markAuto;
+        String classAuto = super.classAuto;
+        Engine engine = super.engine;
+        System.out.println("Марка " + markAuto);
+        System.out.println("Класс " + classAuto);
+        System.out.println("Производитель двигателя" + engine.produser);
+        System.out.println("Мощность двигателя" + engine.power);
     }
 }
