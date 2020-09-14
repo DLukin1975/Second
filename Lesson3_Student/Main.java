3c42a10fe75c585e51209c9332dd6b8312660a65
d.lukin <d.lukin@x5.ru>
2020-09-14 21:31:01 +0300

    Урок 3

--- Lesson3_Student/Main.java
+++ Lesson3_Student/Main.java
@@ -5,12 +5,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Student[] student = {new Student("Dmitriy", "Student", "604", 4.8d),
-                             new Student("Nicolay", "Student", "604", 5d),
-                             new Aspirant("Dmitriy", "Aspirant", "604", 4.8d),
-                             new Aspirant("Sasha", "Aspirant", "604", 5d)};
+                new Student("Nicolay", "Student", "604", 5d),
+                new Aspirant("Dmitriy", "Aspirant", "604", 4.8d),
+                new Aspirant("Sasha", "Aspirant", "604", 5d)};
         int i;
-        for (i=0;i< student.length;i++) {
-            System.out.println("У " + student[i].firstName +"." + student[i].lastName +
+        for (i = 0; i < student.length; i++) {
+            System.out.println("У " + student[i].firstName + "." + student[i].lastName +
                     " средняя оценка " + student[i].averageMark +
                     " стипендия " + student[i].getScholarship());
         }
