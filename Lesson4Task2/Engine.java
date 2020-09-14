9560e511e60cb8706a605152a8b2bfa6327610f9
d.lukin <d.lukin@x5.ru>
2020-09-14 19:02:31 +0300

    Урок 4 задание 2

--- Lesson4Task2/Engine.java
+++ Lesson4Task2/Engine.java
@@ -1,9 +1,9 @@
 package Lesson4Task2;
 
 public class Engine {
-    private String produser;
-    private String power;
-    public Engine (String produser, String power) {
+    public String produser;
+    public Integer power;
+    public Engine (String produser, Integer power) {
         this.produser = produser;
         this.power = power;
     }
--- Lesson4Task2/Lorry.java
+++ Lesson4Task2/Lorry.java
@@ -16,6 +16,11 @@ public class Lorry  extends Car {
         @Override
             public void printinfo () {
             String markAuto = super.markAuto;
+            String classAuto = super.classAuto;
+            Engine engine = super.engine;
             System.out.println("Марка " + markAuto);
+            System.out.println("Класс " + classAuto);
+            System.out.println ("Производитель двигателя" + engine.produser);
+            System.out.println ("Мощность двигателя" + engine.power);
     }
 }
