8ad529fb39f97e67e6b9a0adc51d76fe9085c670
d.lukin <d.lukin@x5.ru>
2020-09-14 21:31:57 +0300

    Урок 3

--- Lesson3_PHONE/Main.java
+++ Lesson3_PHONE/Main.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Phone phone1 = new Phone("N234","Nokia A310",200);
-        Phone phone2 = new Phone("N345","Ericson XXL",550);
-        Phone phone3 = new Phone("N678","Gnusmas 10A",150);
+        Phone phone1 = new Phone("N234", "Nokia A310", 200);
+        Phone phone2 = new Phone("N345", "Ericson XXL", 550);
+        Phone phone3 = new Phone("N678", "Gnusmas 10A", 150);
 
         System.out.println("Созданные экземпляры");
-        System.out.println(phone1.model + " " +phone1.number + " " + phone1.weght);
-        System.out.println(phone2.model + " " +phone2.number + " " + phone2.weght);
-        System.out.println(phone3.model + " " +phone3.number + " " + phone3.weght);
+        System.out.println(phone1.model + " " + phone1.number + " " + phone1.weght);
+        System.out.println(phone2.model + " " + phone2.number + " " + phone2.weght);
+        System.out.println(phone3.model + " " + phone3.number + " " + phone3.weght);
 
         System.out.println();
         System.out.println("Вызов метода getNumber");
@@ -21,9 +21,9 @@ public class Main {
 
         System.out.println();
         System.out.println("Вызов перегруженного метода receiveCall");
-        phone1.receiveCall("Павел","123");
-        phone2.receiveCall("Юрий","234");
-        phone3.receiveCall("Ангелина","345");
+        phone1.receiveCall("Павел", "123");
+        phone2.receiveCall("Юрий", "234");
+        phone3.receiveCall("Ангелина", "345");
 
 
         String[] nums = new String[5];
