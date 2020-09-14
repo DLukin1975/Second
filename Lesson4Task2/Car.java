fc7c15a0bc6a7a67e04e1b53505aa54d5880514a
d.lukin <d.lukin@x5.ru>
2020-09-14 21:20:35 +0300

    Урок 4

--- Lesson4Task2/Car.java
+++ Lesson4Task2/Car.java
@@ -4,18 +4,24 @@ public abstract class Car {
     public String markAuto;
     public String classAuto;
     public Engine engine;
-    public Car (String markAuto, String classAuto, Engine engine) {
+
+    public Car(String markAuto, String classAuto, Engine engine) {
         this.markAuto = markAuto;
         this.classAuto = classAuto;
         this.engine = engine;
     }
+
     abstract void start();
+
     abstract void stop();
+
     abstract void printinfo();
-    void turnleft () {
+
+    void turnleft() {
         System.out.println("Поворот налево");
     }
-    void turnright () {
+
+    void turnright() {
         System.out.println("Поворот направо");
     }
 }
