8ffd2e8dee08555dd87c3545b35630fdbf29b7fc
d.lukin <d.lukin@x5.ru>
2020-09-14 21:21:21 +0300

    Урок 4

--- Lesson4Task1/Rectangle.java
+++ Lesson4Task1/Rectangle.java
@@ -1,14 +1,16 @@
 package Lesson4Task1;
 
-public class Rectangle implements  Shape {
+public class Rectangle implements Shape {
     private double x;
     private double y;
-    public Rectangle (double x, double y) {
-       this.x = x;
-       this.y= y;
+
+    public Rectangle(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
+
     @Override
     public double square() {
-        return this.x*this.y;
+        return this.x * this.y;
     }
 }
