56b9a28d6c0e51b8e0e89241b31723b9f29f8d2b
d.lukin <d.lukin@x5.ru>
2020-09-14 21:33:01 +0300

    Урок 3

--- Lesson3_Animal/Cat.java
+++ Lesson3_Animal/Cat.java
@@ -1,16 +1,17 @@
 package Lesson3_Animal;
 
-public class Cat extends Animal{
+public class Cat extends Animal {
     String catType;
 
     public Cat(String name, String food, String location) {
         super(name, food, location);
     }
-    public void makeNoise(){
+
+    public void makeNoise() {
         System.out.println("кот " + name + " шумит в location " + location);
     }
 
-    public void eat(){
+    public void eat() {
         System.out.println("кот " + name + " ест еду - " + food);
     }
 }
