7e270191ab4db8e8e0211d9af5381455db3a1bf9
d.lukin <d.lukin@x5.ru>
2020-09-14 21:09:27 +0300

    Урок 2 задание 6

--- Lesson2task1.java
+++ Lesson2task1.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 public class Lesson2task1 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println ("Введите трехзначное число");
+        System.out.println("Введите трехзначное число");
         int number = in.nextInt();
-        if (number < 100||number > 999) {
-            System.out.println ("Число не является трехзначным");
+        if (number < 100 || number > 999) {
+            System.out.println("Число не является трехзначным");
 
-        }
-        else {
-            int i = number%10;
+        } else {
+            int i = number % 10;
             System.out.print("Последняя цифра числа - ");
             System.out.println(i);
         }
