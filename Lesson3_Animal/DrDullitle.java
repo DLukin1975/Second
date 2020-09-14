56bd825bcc2e5742b4027c0f78736d5227e209c7
d.lukin <d.lukin@x5.ru>
2020-09-14 21:32:39 +0300

    Урок 3

--- Lesson3_Animal/DrDullitle.java
+++ Lesson3_Animal/DrDullitle.java
@@ -1,7 +1,7 @@
 package Lesson3_Animal;
 
 public class DrDullitle {
-    public void treatAnimal (Animal animal) {
+    public void treatAnimal(Animal animal) {
         System.out.println("На прием пришел " + animal.name + " который ест " + animal.food + " в " + animal.location);
 
     }
