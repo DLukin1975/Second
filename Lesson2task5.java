3bd3f29179a3fde16be614fd3ffa8b2a9f8ca220
d.lukin <d.lukin@x5.ru>
2020-09-14 21:15:16 +0300

    Урок 2 задание 5

--- Lesson2task5.java
+++ Lesson2task5.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class Lesson2task5 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int maxi = 3;
         int i;
         int mini;
         int[] number = new int[maxi];
-
-        for (i=0;i<maxi;i++) {
-            System.out.println ("Введите целое число номер " + i);
+        for (i = 0; i < maxi; i++) {
+            System.out.println("Введите целое число номер " + i);
             number[i] = in.nextInt();
         }
         mini = number[0];
-        for (i=0;i<maxi;i++) {
+        for (i = 0; i < maxi; i++) {
             if (number[i] < mini) {
                 mini = number[i];
             }
