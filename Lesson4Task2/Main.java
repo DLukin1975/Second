7d6ce3ec193a7c6ba95aadba4e1372cf70dec63b
d.lukin <d.lukin@x5.ru>
2020-09-14 21:19:49 +0300

    Урок 4

--- Lesson4Task2/Main.java
+++ Lesson4Task2/Main.java
@@ -2,6 +2,6 @@ package Lesson4Task2;
 
 public class Main {
     public static void main(String[] args) {
-             System.out.println("Класс main");
-      }
+        System.out.println("Класс main");
+    }
 }
