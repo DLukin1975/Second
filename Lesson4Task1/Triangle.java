bc0c95127da7967c86fa2b5188ada7f4a69eff38
d.lukin <d.lukin@x5.ru>
2020-09-14 21:20:53 +0300

    Урок 4

--- Lesson4Task1/Triangle.java
+++ Lesson4Task1/Triangle.java
@@ -4,12 +4,13 @@ public class Triangle implements Shape {
     private double h;
     private double a;
 
-    public Triangle (double h, double a) {
+    public Triangle(double h, double a) {
         this.h = h;
         this.a = a;
     }
+
     @Override
     public double square() {
-        return this.a*this.h/2;
+        return this.a * this.h / 2;
     }
 }
