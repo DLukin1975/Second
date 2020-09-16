1a6a193bc29365ca8b6032d83093e60a9814b5e5
d.lukin <d.lukin@x5.ru>
2020-09-16 20:41:11 +0300

    Урок 5

--- Lesson5Task1/ConsoleLogger.java
+++ Lesson5Task1/ConsoleLogger.java
@@ -1,8 +1,8 @@
 package Lesson5Task1;
 
-public class ConsoleLogger implements   Logger{
+public class ConsoleLogger implements Logger {
     @Override
-    public void log (String logMessage) {
-        System.out.println("vjvj");
+    public void log(String logMessage) {
+        System.out.println("Пишем в консоль сообщение: " + logMessage);
     }
 }
