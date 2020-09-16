7cafc89a81e6b10b63f9ebaff7ddcd5be5e828bf
d.lukin <d.lukin@x5.ru>
2020-09-16 21:23:59 +0300

    Урок 5

---
+++ Lesson5Task1/Main.java
@@ -0,0 +1,10 @@
+package Lesson5Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        double x = 40d;
+        double y = 5;
+        Calc calc = new Calc();
+        System.out.println(calc.addition(x, y));
+    }
+}
