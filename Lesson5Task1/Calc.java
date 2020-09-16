dd5bb8aa0b683ae1bf06d933a77bcd0fb04f006e
d.lukin <d.lukin@x5.ru>
2020-09-16 21:20:45 +0300

    Урок 5

--- Lesson5Task1/Calc.java
+++ Lesson5Task1/Calc.java
@@ -1,7 +1,19 @@
 package Lesson5Task1;
 
 public class Calc {
-    public double add (double x, double y) {
-        return x+y;
+    public double addition(double x, double y) {
+        return x + y;
+    }
+
+    public double substruction(double x, double y) {
+        return x - y;
+    }
+
+    public double multiplication(double x, double y) {
+        return x * y;
+    }
+
+    public double division(double x, double y) {
+        return x / y;
     }
 }
