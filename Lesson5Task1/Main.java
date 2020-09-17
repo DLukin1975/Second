a9de539a39cac68cd429fcc7271afb372a8d6732
d.lukin <d.lukin@x5.ru>
2020-09-17 19:52:50 +0300

    Урок 5

--- Lesson5Task1/Main.java
+++ Lesson5Task1/Main.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как будем логировать (Db/File/Console)");
+        System.out.println("Как будем логировать (Db/File/Console)?");
         Scanner in = new Scanner(System.in);
         LogType logName = null;
-        //logName = LogType.valueOf(in.nextLine());
 
         try { logName = LogType.valueOf(in.nextLine());
         } catch (Exception e) {
             System.out.println("Нет такого типа логирования");
             System.exit(0);
         }
-        //System.out.println(char(logName));
+
         Logger log = null;
+
         switch (logName) {
             case Db: log = new DbLogger();
             break;
@@ -25,9 +25,12 @@ public class Main {
             break;
             default:
         }
-        double x = 40d;
-        double y = 5;
-        //Logger log = new DbLogger();
+
+        System.out.println("Введите первое значение");
+        double x = in.nextInt();
+        System.out.println("Введите второе значение");
+        double y = in.nextInt();
+
         Calc calc = new Calc();
         double z = calc.addition(x, y, log);
     }
