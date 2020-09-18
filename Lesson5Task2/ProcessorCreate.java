2cc4f06cfd60efadacb6f2dec78aa0eea8b6cb46
d.lukin <d.lukin@x5.ru>
2020-09-18 22:41:46 +0300

    Урок 5

---
+++ Lesson5Task2/ProcessorCreate.java
@@ -0,0 +1,8 @@
+package Lesson5Task2;
+
+public class ProcessorCreate extends CharactiristicCreate {
+    @Override
+    public Charactiristic createCharacteristic() {
+        return new Processor();
+    }
+}
