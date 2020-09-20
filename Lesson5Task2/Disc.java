d529ae33f35fa7ff247b0cd39aa0061c65e9498d
d.lukin <d.lukin@x5.ru>
2020-09-20 16:24:40 +0300

    Урок 5

---
+++ Lesson5Task2/Disc.java
@@ -0,0 +1,11 @@
+package Lesson5Task2;
+
+public class Disc implements Charactiristic {
+    @Override
+    public void character() {
+        String diskProduser = "";
+        Integer diskVolume = 0;
+        String diskType = "";
+        String diskFormfactor = "";
+    }
+}
