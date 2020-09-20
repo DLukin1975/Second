b6e5764d7fc81c244b40699c3c2aceca925a63e0
d.lukin <d.lukin@x5.ru>
2020-09-20 16:25:37 +0300

    Урок 5

---
+++ Lesson5Task2/DiscCreate.java
@@ -0,0 +1,8 @@
+package Lesson5Task2;
+
+public class DiscCreate  extends CharactiristicCreate {
+    @Override
+    public Charactiristic createCharacteristic() {
+        return new Disc();
+    }
+}
