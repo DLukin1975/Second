273009ab3c979567e70884200d97c2791ce5702f
d.lukin <d.lukin@x5.ru>
2020-09-20 18:29:20 +0300

    Урок 5

---
+++ Lesson5Task2/Main.java
@@ -0,0 +1,12 @@
+package Lesson5Task2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CharactiristicCreate[] specification = new CharactiristicCreate[] {
+        };
+        TwoMonitorStrategy strategy = new TwoMonitorStrategy();
+        strategy.twoMonitorStrategy(specification);
+    }
+    }
