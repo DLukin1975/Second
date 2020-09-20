a9031aaa331409e64fda8f3acc9cb25ccff313c0
d.lukin <d.lukin@x5.ru>
2020-09-20 16:33:29 +0300

    Урок 5

---
+++ Lesson5Task2/MonitorCreate.java
@@ -0,0 +1,8 @@
+package Lesson5Task2;
+
+public class MonitorCreate extends CharactiristicCreate {
+    @Override
+    public Charactiristic createCharacteristic() {
+        return new Monitor();
+    }
+}
