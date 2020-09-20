dda432cf64f1999433b177fabc74ee75d0af9274
d.lukin <d.lukin@x5.ru>
2020-09-20 18:20:26 +0300

    Урок 5

--- Lesson5Task2/OneMonitorStrategy.java
+++ Lesson5Task2/OneMonitorStrategy.java
@@ -1,12 +1,11 @@
 package Lesson5Task2;
 
 public class OneMonitorStrategy {
-    public void oneMonitorStrategy (CharactiristicCreate[] specification) {
+    public void oneMonitorStrategy(CharactiristicCreate[] specification) {
         specification[0] = new ProcessorCreate();
         specification[1] = new RamCreate();
         specification[2] = new VideocardCreate();
         specification[3] = new DiscCreate();
         specification[4] = new MonitorCreate();
     }
-
 }
