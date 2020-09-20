ba27960f4a5b02ab7fb1dd55e5c80144860e9015
d.lukin <d.lukin@x5.ru>
2020-09-20 16:26:36 +0300

    Урок 5

--- Lesson5Task2/Processor.java
+++ Lesson5Task2/Processor.java
@@ -3,9 +3,9 @@ package Lesson5Task2;
 public class Processor implements Charactiristic {
     @Override
     public void character() {
-        Integer frequrency = 0;
-        int solidNum = 0;
-        Integer cash = 0;
-        String produser = "";
+        Integer frequrencyProcessor = 0;
+        int solidNumProcessor = 0;
+        Integer cashProcessor = 0;
+        String produserProcessor = "";
     }
 }
