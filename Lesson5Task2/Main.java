e77184d8f51ec5e5e090f46c872bae4d3b2b469f
d.lukin <d.lukin@x5.ru>
2020-09-20 18:30:58 +0300

    Урок 5

--- Lesson5Task2/Main.java
+++ Lesson5Task2/Main.java
@@ -1,12 +1,13 @@
 package Lesson5Task2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CharactiristicCreate[] specification = new CharactiristicCreate[] {
-        };
+        CharactiristicCreate[] specification = new CharactiristicCreate[]{};
         TwoMonitorStrategy strategy = new TwoMonitorStrategy();
         strategy.twoMonitorStrategy(specification);
+        System.out.println(Arrays.toString(specification));
     }
-    }
+}
