162de7f5bd098c6da6ed21bd62478a7ef47c8a22
d.lukin <d.lukin@x5.ru>
2020-09-20 19:30:05 +0300

    Урок 5

--- Lesson5Task2/Computer.java
+++ Lesson5Task2/Computer.java
@@ -3,6 +3,7 @@ package Lesson5Task2;
 public class Computer {
     private CharactiristicCreate[] specification;
     private int i;
+    public Charactiristic[] charactiristic;
     public void createComputer(CharactiristicCreate[] specification) {
         for (i=0; i<specification.length;i++) {
             specification[i].createCharacteristic();
