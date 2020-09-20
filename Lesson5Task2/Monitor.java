985ca0ccf20a909d7ea0d7dccd6654c2eeca2c82
d.lukin <d.lukin@x5.ru>
2020-09-20 16:32:22 +0300

    Урок 5

---
+++ Lesson5Task2/Monitor.java
@@ -0,0 +1,9 @@
+package Lesson5Task2;
+
+public class Monitor implements Charactiristic {
+    @Override
+    public void character() {
+        Integer diagonalNomitor = 0;
+        String typeMonitop = "";
+    }
+}
