12b3f6cc79052355a3d062c1c93a56597b7e6531
d.lukin <d.lukin@x5.ru>
2020-09-20 21:30:05 +0300

    Урок 5

--- Lesson5Task2/Main.java
+++ Lesson5Task2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         CharactiristicCreate[] specification;
         SpecificationGenerator specificationGenerator = new SpecificationGenerator();
-        specificationGenerator.setStrategyCreateSpecification(new TwoMonitorStrategy());
+        specificationGenerator.setStrategyCreateSpecification(new OneMonitorStrategy());
         specification = specificationGenerator.getSpecification();
         System.out.println("Результат: " + specification[0]);
         System.out.println("Результат: " + specification[1]);
