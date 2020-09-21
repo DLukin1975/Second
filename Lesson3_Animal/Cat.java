7f9fa7e4e0e1e58854d6890fc4a328ab61c3b0b1
d.lukin <d.lukin@x5.ru>
2020-09-21 18:58:54 +0300

    Урок 5

--- Lesson3_Animal/Cat.java
+++ Lesson3_Animal/Cat.java
@@ -1,7 +1,7 @@
 package Lesson3_Animal;
 
 public class Cat extends Animal {
-    String catType;
+    public String catType;
 
     public Cat(String name, String food, String location) {
         super(name, food, location);
