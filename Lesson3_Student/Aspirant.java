f0d21d06640fbfbf60de8a1b143ef995dd0713d4
d.lukin <d.lukin@x5.ru>
2020-09-21 19:57:14 +0300

    Урок 3

--- Lesson3_Student/Aspirant.java
+++ Lesson3_Student/Aspirant.java
@@ -1,7 +1,7 @@
 package Lesson3_Student;
 
 public class Aspirant extends Student {
-    String aspirantWork;
+    public String aspirantWork;
 
     public Aspirant(String firstName, String lastName, String group, double averageMark) {
         super(firstName, lastName, group, averageMark);
