70251c459dbbbe2d27b71fb0a6307ec110d94999
d.lukin <d.lukin@x5.ru>
2020-09-21 19:53:44 +0300

    Урок 3

--- Lesson3_PHONE/Main.java
+++ Lesson3_PHONE/Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class Main {
     private static Phone[] phone = new Phone[3];
+
     public static void main(String[] args) {
         phone[0] = new Phone("N234", "Nokia A310", 200);
         phone[1] = new Phone("N345", "Ericson XXL", 550);
@@ -13,18 +14,12 @@ public class Main {
         for (Phone value : phone) {
             System.out.println(value.model + " " + value.number + " " + value.weght);
         }
- //       System.out.println(phone[0].model + " " + phone[0].number + " " + phone[0].weght);
- //       System.out.println(phone[1].model + " " + phone[1].number + " " + phone[1].weght);
-  //      System.out.println(phone[2].model + " " + phone[2].number + " " + phone[2].weght);
 
         System.out.println();
         System.out.println("Вызов метода getNumber");
         for (Phone value : phone) {
             value.getNumber();
         }
-      //  phone[0].getNumber();
-     //   phone[1].getNumber();
-      //  phone[2].getNumber();
 
         System.out.println();
         System.out.println("Вызов перегруженного метода receiveCall");
