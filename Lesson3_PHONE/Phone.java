e54a2bdd9b0ba848e66de468af2ded61f39bd4a7
d.lukin <d.lukin@x5.ru>
2020-09-21 19:04:41 +0300

    Урок 3

--- Lesson3_PHONE/Phone.java
+++ Lesson3_PHONE/Phone.java
@@ -2,6 +2,10 @@ package Lesson3_PHONE;
 
 public class Phone {
 
+    public String number;
+    public String model;
+    public int weght;
+
     public Phone(String number, String model) {
         this.number = number;
         this.model = model;
@@ -14,10 +18,6 @@ public class Phone {
 
     public Phone() {
     }
-
-    String number;
-    String model;
-    int weght;
 
     public void receiveCall(String name) {
         System.out.println("Звонит " + name);
@@ -32,9 +32,8 @@ public class Phone {
     }
 
     public void sendMessage(String[] nums) {
-        int i;
-        for (i = 0; i < nums.length; i++) {
-            System.out.println("Сообщение будет отправлено номеру " + nums[i]);
+        for (String num : nums) {
+            System.out.println("Сообщение будет отправлено номеру " + num);
         }
     }
 }
