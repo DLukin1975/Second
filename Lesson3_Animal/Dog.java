149abbf64ee276c37acac26894b378b36039e2fe
d.lukin <d.lukin@x5.ru>
2020-09-21 19:01:54 +0300

    Урок 5

--- Lesson3_Animal/Dog.java
+++ Lesson3_Animal/Dog.java
@@ -1,7 +1,7 @@
 package Lesson3_Animal;
 
 public class Dog extends Animal {
-    String dogType;
+    public String dogType;
 
     public Dog(String name, String food, String location) {
         super(name, food, location);
--- Lesson3_Animal/Horse.java
+++ Lesson3_Animal/Horse.java
@@ -1,7 +1,7 @@
 package Lesson3_Animal;
 
 public class Horse extends Animal {
-    String horseType;
+    public String horseType;
 
     public Horse(String name, String food, String location) {
         super(name, food, location);
