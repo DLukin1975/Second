6bc92e98a7cb1a502158d41fdff2434b76e7f50f
d.lukin <d.lukin@x5.ru>
2020-09-21 18:50:43 +0300

    Урок 5

--- Lesson3_Animal/Animal.java
+++ Lesson3_Animal/Animal.java
@@ -1,25 +1,27 @@
 package Lesson3_Animal;
 
 public class Animal {
+
+    public String name;
+    public String food;
+    public String location;
+    private int Age;
+
     public Animal(String name, String food, String location) {
         this.name = name;
         this.food = food;
         this.location = location;
     }
-    String name ;
-    String food;
-    String location;
-    int Age;
 
-    public void makeNoise(){
+    public void makeNoise() {
         System.out.println(name + " шумит в location " + location);
     }
 
-    public void eat(){
+    public void eat() {
         System.out.println(name + " ест еду - " + food);
     }
 
-    public void sleep(){
+    public void sleep() {
         System.out.println(name + " спит");
     }
 }
