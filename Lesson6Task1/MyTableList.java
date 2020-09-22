50f244149765d0572a29002459fca7f4904adb26
d.lukin <d.lukin@x5.ru>
2020-09-22 19:36:19 +0300

    Урок 6

--- Lesson6Task1/MyTableList.java
+++ Lesson6Task1/MyTableList.java
@@ -3,5 +3,7 @@ package Lesson6Task1;
 import java.lang.reflect.Array;
 import java.util.*;
 
-public class MyTableList extends ArrayList<String> {
+public class MyTableList<String> extends ArrayList<String> {
+ //   @Override
+
 }
