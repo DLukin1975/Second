1cb1ecd54ac14cad9c5077cc31b2b179f3684056
d.lukin <d.lukin@x5.ru>
2020-09-22 20:44:42 +0300

    Урок 6

--- Lesson6Task1/Main.java
+++ Lesson6Task1/Main.java
@@ -3,8 +3,8 @@ package Lesson6Task1;
 //import java.util.*;
 
 public class Main {
-    public void main(String[] args) {
-
+    public static void main(String[] args) {
+MyTableList<String> list1 = new MyTableList<>();
         System.out.print("Элемент1");
     }
 }
