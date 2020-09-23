e9d2709ef6dba060fa796bd6034b60024f0f0f89
d.lukin <d.lukin@x5.ru>
2020-09-23 21:10:06 +0300

    Урок 6

--- Lesson6Task1/MyArrayList.java
+++ Lesson6Task1/MyArrayList.java
@@ -10,6 +10,7 @@ public class MyArrayList<E> implements MyList<E> {
 
     int size = 0;
     Object[] list = new Object[0];
+    Object[] pocket;
 
     @Override
     public int size() {
@@ -18,7 +19,7 @@ public class MyArrayList<E> implements MyList<E> {
 
     @Override
     public void add(E item) {
-        Object[] pocket = new Object[size + 1];
+        pocket = new Object[size + 1];
         if (size == 0) {
             size = 1;
             list = new Object[1];
@@ -29,14 +30,26 @@ public class MyArrayList<E> implements MyList<E> {
             pocket[pocket.length - 1] = item;
             size = size + 1;
             list = new Object[size];
-            System.out.println("текущее " + list.length);
+            //System.out.println("текущее " + list.length);
             IntStream.range(0, size).forEach(i -> list[i] = pocket[i]);
         }
     }
 
     @Override
     public void remove(int index) {
-
+        if (index < size & index >= 0) {
+            pocket = new Object[size - 1];
+            for (int i = 0; i < size - 1; i++) {
+                if (i < index) {
+                    pocket[i] = list[i];
+                } else {
+                    pocket[i] = list[i + 1];
+                }
+            }
+            size = size - 1;
+            list = new Object[size];
+            IntStream.range(0, size).forEach(i -> list[i] = pocket[i]);
+        }
     }
 
     @Override
