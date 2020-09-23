585a7e8e4bd3996f09d194c877e1481457d15d8e
d.lukin <d.lukin@x5.ru>
2020-09-23 18:59:46 +0300

    Урок 6

--- Lesson6Task1/MyArrayList.java
+++ Lesson6Task1/MyArrayList.java
@@ -3,5 +3,16 @@ package Lesson6Task1;
 import java.util.ArrayList;
 
 public class MyArrayList<E>  extends ArrayList<E> {
+private int size = 0;
 
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean add(E item) {
+        size = size+1;
+        return false;
+    }
 }
