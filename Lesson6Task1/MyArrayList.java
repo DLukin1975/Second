05a5ec1b55921502ab04b796b803a44fabcf930e
d.lukin <d.lukin@x5.ru>
2020-09-23 20:06:40 +0300

    Урок 6

--- Lesson6Task1/MyArrayList.java
+++ Lesson6Task1/MyArrayList.java
@@ -5,6 +5,38 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class MyArrayList<E>  {
+public class MyArrayList<E>  implements MyList<E> {
 
+    int size = 0;
+    Object[] list = new Object[0];
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void remove(E item) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
