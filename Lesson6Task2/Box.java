3208c4ae743069bb32a8942f43ff1a4d4724adef
d.lukin <d.lukin@x5.ru>
2020-09-24 20:25:17 +0300

    Урок 6

---
+++ Lesson6Task2/Box.java
@@ -0,0 +1,14 @@
+package Lesson6Task2;
+
+import java.util.List;
+
+public class Box {
+    private List<Fruit> content;
+
+    public Box(List<Fruit> content) {
+        this.content = content;
+    }
+    public Box() {
+
+    }
+}
--- Lesson6Task2/Main.java
+++ Lesson6Task2/Main.java
@@ -3,6 +3,8 @@ package Lesson6Task2;
 public class Main{
     public static void main(String[] args) {
 
-        System.out.print("Элемент1");
+        Box box1 = new Box();
+        Box box2 = new Box();
+
     }
 }
