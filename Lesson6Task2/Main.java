496348ded2f8faabe7de9cf0a8455ea11d7c7bc4
d.lukin <d.lukin@x5.ru>
2020-09-24 20:47:34 +0300

    Урок 6

--- Lesson6Task2/Main.java
+++ Lesson6Task2/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
         Box box1 = new Box();
         Box box2 = new Box();
-        box1.addFruit(new Apple());
-        box1.addFruit(new Apple());
+        Apple apple = new Apple();
+        box1.addFruit(apple);
+    //    box1.addFruit(new Apple());
         System.out.println(box1.getWeight());
     }
 }
