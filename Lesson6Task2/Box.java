85b60cf29a1ec90a934face278bb21ab267d6f41
d.lukin <d.lukin@x5.ru>
2020-09-24 20:32:00 +0300

    Урок 6

--- Lesson6Task2/Box.java
+++ Lesson6Task2/Box.java
@@ -3,12 +3,15 @@ package Lesson6Task2;
 import java.util.List;
 
 public class Box {
-    private List<Fruit> content;
+    public List<Fruit> content;
 
     public Box(List<Fruit> content) {
         this.content = content;
     }
     public Box() {
-
     }
+    public void addFruit (Fruit fruit) {
+        content.add(fruit);
+    }
+
 }
--- Lesson6Task2/Main.java
+++ Lesson6Task2/Main.java
@@ -1,10 +1,12 @@
 package Lesson6Task2;
 
-public class Main{
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
 
         Box box1 = new Box();
         Box box2 = new Box();
-
+        box1.addFruit(new Apple());
     }
 }
