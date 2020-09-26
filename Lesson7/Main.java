c5b2017cb8c0f884e5119c4779a17c8b8c4b665d
d.lukin <d.lukin@x5.ru>
2020-09-26 18:34:26 +0300

    Урок 7

--- Lesson7/Main.java
+++ Lesson7/Main.java
@@ -6,20 +6,22 @@ import java.nio.file.Path;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
         Path path = Path.of("AmountStorage.txt");
 
         try {
-
             Files.createFile(path);
-        } catch
-        (IOException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        {
+            System.out.println("Файл AmountStorage.txt уже существует");
         }
         System.out.println(Files.isRegularFile(path));
         System.out.println("Готово");
-        Files.delete(path);
+        //      Files.delete(path);
         System.out.println(Files.isRegularFile(path));
     }
 }
