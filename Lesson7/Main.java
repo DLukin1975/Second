196d018ededb9c336000eb91bdb0c0d055e589a5
d.lukin <d.lukin@x5.ru>
2020-09-26 18:07:16 +0300

    Урок 7

--- Lesson7/Main.java
+++ Lesson7/Main.java
@@ -6,9 +6,9 @@ import java.nio.file.Path;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-  //      Files.createFile(Path.of("c:\Q123.txt"));
+        Files.createFile(Path.of("C:\\readme.txt"));
 
         System.out.println(Files.isRegularFile(Path.of("C:\\readme.txt")));
         System.out.println("Готово");
