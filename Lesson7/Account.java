d90fb64dd2abef254aa185042f400499c5426b9f
d.lukin <d.lukin@x5.ru>
2020-09-26 16:12:02 +0300

    Урок 7

---
+++ Lesson7/Account.java
@@ -0,0 +1,16 @@
+package Lesson7;
+
+
+
+public class Account {
+
+    private Integer id ;
+    private String holder ;
+    private double amount = 0;
+
+
+    public  Account(Integer id, String holder) {
+    this.id = id;
+    this.holder = holder; }
+
+}
