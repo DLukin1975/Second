d6415bb57edb965a571b6db39758ad67396dcecb
d.lukin <d.lukin@x5.ru>
2020-09-26 15:29:56 +0300

    Урок 6

--- Lesson6Task2/Fruit.java
+++ Lesson6Task2/Fruit.java
@@ -2,6 +2,8 @@ package Lesson6Task2;
 
 public abstract class Fruit {
     private double weight = 0d;
+    public String type = "Fruit";
+
     public double getWeightFruit() {
         return weight;
     }
